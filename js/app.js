5f1dd31d918c2f01c7099b9c8519bc39924b478e
joproelofs-cpu <joproelofs@gmail.com>
2026-09-14 00:43:42 +0200

    Hoog versienummer op zodat de nieuwe trolley-foto direct zichtbaar is

--- js/app.js
+++ js/app.js
@@ -106,7 +106,7 @@ function setCatalogueTheme(on){
   if (on){
     if (!link){
       link = document.createElement('link');
-      link.rel = 'stylesheet'; link.href = 'css/mc.css?v=6';
+      link.rel = 'stylesheet'; link.href = 'css/mc.css?v=7';
       link.dataset.injected = 'true';
       document.head.appendChild(link);
     }
